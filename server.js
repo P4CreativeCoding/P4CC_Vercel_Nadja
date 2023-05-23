afa26c0bda0e80f60f599514a20d6d1dafc77924
Nadja Jung <nadja.jung@stud.h-da.de>
2023-05-23 11:58:03 +0200

    neuer versuch

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
+let password = "NADJA"; // Initialwert für das Passwort
+
+app.use(express.json()); // Middleware zum Parsen von JSON-Daten
+
+// Endpunkt zum Setzen des Passworts
+app.post("/setPassword", (req, res) => {
+  const { newPassword } = req.body;
+  password = newPassword;
+  res.sendStatus(200);
+});
+
+// Endpunkt zum Überprüfen des Passworts
+app.post("/checkPassword", (req, res) => {
+  const { enteredPassword } = req.body;
+  if (enteredPassword === password) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
 // Statische Dateien bereitstellen
 app.use(express.static(__dirname + "/public"));
 
