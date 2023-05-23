28c04c562938d15231fd2c64ae8ae81f3cdbff41
Nadja Jung <nadja.jung@stud.h-da.de>
2023-05-23 11:27:53 +0200

    damits in vercel geht

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Statische Dateien bereitstellen
 app.use(express.static(__dirname + "/public"));
