5e41bb34a3ba5fde9e43f51132cc7f87153dcd0b
Nadja Jung <nadja.jung@stud.h-da.de>
2023-05-23 11:22:29 +0200

    Auto rennen

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const port = 3000;
+
+// Statische Dateien bereitstellen
+app.use(express.static(__dirname + "/public"));
+
+// WebSocket-Verbindung herstellen
+io.on("connection", (socket) => {
+  console.log("Ein Benutzer hat sich verbunden");
+
+  // Event-Handler für Bewegungsdaten des roten Autos
+  socket.on("redCarMovement", (movement) => {
+    socket.broadcast.emit("redCarMovement", movement);
+  });
+
+  // Event-Handler für Bewegungsdaten des blauen Autos
+  socket.on("blueCarMovement", (movement) => {
+    socket.broadcast.emit("blueCarMovement", movement);
+  });
+
+  // Event-Handler für Verbindungsabbruch
+  socket.on("disconnect", () => {
+    console.log("Ein Benutzer hat die Verbindung getrennt");
+  });
+});
+
+// Server starten
+server.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
